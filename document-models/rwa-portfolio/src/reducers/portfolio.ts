235e871fb7fa9fa88b96d67aaf0b929e9d12a525
ryanwolhuter <dev@ryanwolhuter.com>
2023-12-22 12:42:38 +0200

    feat: add portfolio reducers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/rwa-portfolio/src/reducers/portfolio.ts
+++ document-models/rwa-portfolio/src/reducers/portfolio.ts
@@ -1,34 +1,138 @@
 /**
-* This is a scaffold file meant for customization: 
-* - modify it by implementing the reducer functions
-* - delete the file and run the code generator again to have it reset
-*/
+ * This is a scaffold file meant for customization:
+ * - modify it by implementing the reducer functions
+ * - delete the file and run the code generator again to have it reset
+ */
 
+import { RwaAsset, RwaCash, RwaFixedIncome } from '../..';
 import { RwaPortfolioPortfolioOperations } from '../../gen/portfolio/operations';
 
 export const reducer: RwaPortfolioPortfolioOperations = {
     addRwaFixedIncomeAssetOperation(state, action, dispatch) {
-        // TODO: Implement "addRwaFixedIncomeAssetOperation" reducer
-        throw new Error('Reducer "addRwaFixedIncomeAssetOperation" not yet implemented');
+        if (
+            !action.input.id ||
+            !action.input.type ||
+            !action.input.name ||
+            !action.input.maturity
+        ) {
+            throw new Error('Id, type, name, and maturity are required');
+        }
+
+        if (isNaN(new Date(action.input.maturity).getTime())) {
+            throw new Error(`Invalid maturity date: ${action.input.maturity}`);
+        }
+
+        if (
+            !state.fixedIncomeTypes.find(
+                asset => asset.id === action.input.type,
+            )
+        ) {
+            throw new Error(`Asset type ${action.input.type} not found`);
+        }
+
+        state.portfolio.push({
+            ...action.input,
+        } as RwaAsset);
     },
     addRwaCashAssetOperation(state, action, dispatch) {
-        // TODO: Implement "addRwaCashAssetOperation" reducer
-        throw new Error('Reducer "addRwaCashAssetOperation" not yet implemented');
+        if (Object.values(action.input).some(value => !value)) {
+            throw new Error('Id, spv and currency are required');
+        }
+
+        if (!state.spvs.find(spv => spv.id === action.input.spv)) {
+            throw new Error(`SPV ${action.input.spv} not found`);
+        }
+
+        state.portfolio.push({
+            ...action.input,
+        } as RwaAsset);
     },
     editRwaFixedIncomeAssetOperation(state, action, dispatch) {
-        // TODO: Implement "editRwaFixedIncomeAssetOperation" reducer
-        throw new Error('Reducer "editRwaFixedIncomeAssetOperation" not yet implemented');
+        if (
+            !action.input.id ||
+            !action.input.type ||
+            !action.input.name ||
+            !action.input.maturity
+        ) {
+            throw new Error('Id, type, name, and maturity are required');
+        }
+        const asset = state.portfolio.find(
+            asset => asset.id === action.input.id,
+        ) as RwaFixedIncome | undefined;
+        if (!asset) {
+            throw new Error(`Asset with id ${action.input.id} not found`);
+        }
+
+        if (isNaN(new Date(action.input.maturity).getTime())) {
+            throw new Error(`Invalid maturity date: ${action.input.maturity}`);
+        }
+
+        if (
+            !state.fixedIncomeTypes.find(
+                asset => asset.id === action.input.type,
+            )
+        ) {
+            throw new Error(`Asset type ${action.input.type} not found`);
+        }
+
+        asset.type = action.input.type;
+        asset.name = action.input.name;
+        asset.ISIN = action.input.ISIN ?? asset.ISIN;
+        asset.CUSIP = action.input.CUSIP ?? asset.CUSIP;
+        asset.coupon = action.input.coupon ?? asset.coupon;
+        asset.maturity = action.input.maturity;
     },
     editRwaCashAssetOperation(state, action, dispatch) {
-        // TODO: Implement "editRwaCashAssetOperation" reducer
-        throw new Error('Reducer "editRwaCashAssetOperation" not yet implemented');
+        if (!action.input.id || !action.input.spv || !action.input.currency) {
+            throw new Error('Id, spv and currency are required');
+        }
+
+        const asset = state.portfolio.find(
+            asset => asset.id === action.input.id,
+        ) as RwaCash | undefined;
+        if (!asset) {
+            throw new Error(`Asset with id ${action.input.id} not found`);
+        }
+
+        if (!state.spvs.find(spv => spv.id === action.input.spv)) {
+            throw new Error(`SPV ${action.input.spv} not found`);
+        }
+
+        asset.spv = action.input.spv;
+        asset.currency = action.input.currency;
     },
     deleteRwaFixedIncomeAssetOperation(state, action, dispatch) {
-        // TODO: Implement "deleteRwaFixedIncomeAssetOperation" reducer
-        throw new Error('Reducer "deleteRwaFixedIncomeAssetOperation" not yet implemented');
+        if (!action.input.id) {
+            throw new Error('Id is required');
+        }
+
+        const asset = state.portfolio.find(
+            asset => asset.id === action.input.id,
+        );
+
+        if (!asset) {
+            throw new Error(`Asset with id ${action.input.id} not found`);
+        }
+
+        state.portfolio = state.portfolio.filter(
+            asset => asset.id !== action.input.id,
+        );
     },
     deleteRwaCashAssetOperation(state, action, dispatch) {
-        // TODO: Implement "deleteRwaCashAssetOperation" reducer
-        throw new Error('Reducer "deleteRwaCashAssetOperation" not yet implemented');
+        if (!action.input.id) {
+            throw new Error('Id is required');
+        }
+
+        const asset = state.portfolio.find(
+            asset => asset.id === action.input.id,
+        );
+
+        if (!asset) {
+            throw new Error(`Asset with id ${action.input.id} not found`);
+        }
+
+        state.portfolio = state.portfolio.filter(
+            asset => asset.id !== action.input.id,
+        );
     },
-}
+};
