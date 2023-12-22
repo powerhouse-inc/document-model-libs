4958737070a329fe8c3d1c432d5b8566aaf2d230
ryanwolhuter <dev@ryanwolhuter.com>
2023-12-22 12:10:22 +0200

    feat: add general reducers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/rwa-portfolio/src/reducers/general.ts
+++ document-models/rwa-portfolio/src/reducers/general.ts
@@ -1,34 +1,76 @@
 /**
-* This is a scaffold file meant for customization: 
-* - modify it by implementing the reducer functions
-* - delete the file and run the code generator again to have it reset
-*/
+ * This is a scaffold file meant for customization:
+ * - modify it by implementing the reducer functions
+ * - delete the file and run the code generator again to have it reset
+ */
 
 import { RwaPortfolioGeneralOperations } from '../../gen/general/operations';
 
+// TODO: Implement "isValidSpvName" function
+function isValidSpvName(potentialName: string) {
+    return potentialName.length > 0;
+}
+
+// TODO: Implement "isValidServiceProviderName" function
+function isValidServiceProviderName(potentialName: string) {
+    return potentialName.length > 0;
+}
+
+// TODO: Implement "isValidServiceProviderCategory" function
+function isValidServiceProviderCategory(potentialCategory: string) {
+    return potentialCategory.length > 0;
+}
+
 export const reducer: RwaPortfolioGeneralOperations = {
     createSpvOperation(state, action, dispatch) {
-        // TODO: Implement "createSpvOperation" reducer
-        throw new Error('Reducer "createSpvOperation" not yet implemented');
+        if (!action.input.id || !action.input.name) {
+            throw new Error('Id and name are required');
+        }
+
+        if (state.spvs.find(spv => spv.id === action.input.id)) {
+            throw new Error(`SPV with id ${action.input.id} already exists!`);
+        }
+
+        if (!isValidSpvName(action.input.name)) {
+            throw new Error(`Invalid SPV name: ${action.input.name}`);
+        }
+
+        state.spvs.push({
+            ...action.input,
+        });
     },
     editSpvOperation(state, action, dispatch) {
-        // TODO: Implement "editSpvOperation" reducer
-        throw new Error('Reducer "editSpvOperation" not yet implemented');
+        const spv = state.spvs.find(spv => spv.id === action.input.id);
+        if (!spv) {
+            throw new Error(`SPV with id ${action.input.id} not found`);
+        }
+
+        if (!isValidSpvName(action.input.name)) {
+            throw new Error(`Invalid SPV name: ${action.input.name}`);
+        }
+
+        spv.name = action.input.name;
     },
     deleteSpvOperation(state, action, dispatch) {
-        // TODO: Implement "deleteSpvOperation" reducer
-        throw new Error('Reducer "deleteSpvOperation" not yet implemented');
+        const spv = state.spvs.find(spv => spv.id === action.input.id);
+        if (!spv) {
+            throw new Error(`SPV with id ${action.input.id} not found`);
+        }
+        state.spvs = state.spvs.filter(spv => spv.id !== action.input.id);
     },
     addServiceProviderOperation(state, action, dispatch) {
-        // TODO: Implement "addServiceProviderOperation" reducer
-        throw new Error('Reducer "addServiceProviderOperation" not yet implemented');
+        // TODO: ServiceProvider type must be added and state needs serviceProviders array
     },
     editServiceProviderOperation(state, action, dispatch) {
         // TODO: Implement "editServiceProviderOperation" reducer
-        throw new Error('Reducer "editServiceProviderOperation" not yet implemented');
+        throw new Error(
+            'Reducer "editServiceProviderOperation" not yet implemented',
+        );
     },
     deleteServiceProviderOperation(state, action, dispatch) {
         // TODO: Implement "deleteServiceProviderOperation" reducer
-        throw new Error('Reducer "deleteServiceProviderOperation" not yet implemented');
+        throw new Error(
+            'Reducer "deleteServiceProviderOperation" not yet implemented',
+        );
     },
-}
+};
