7312eb7b0568c9d031dd62ed879cb13f3d82e071
ryanwolhuter <dev@ryanwolhuter.com>
2023-12-22 13:43:27 +0200

    feat: add attachments reducers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/rwa-portfolio/src/reducers/attachments.ts
+++ document-models/rwa-portfolio/src/reducers/attachments.ts
@@ -1,22 +1,85 @@
 /**
-* This is a scaffold file meant for customization: 
-* - modify it by implementing the reducer functions
-* - delete the file and run the code generator again to have it reset
-*/
+ * This is a scaffold file meant for customization:
+ * - modify it by implementing the reducer functions
+ * - delete the file and run the code generator again to have it reset
+ */
 
 import { RwaPortfolioAttachmentsOperations } from '../../gen/attachments/operations';
 
+function isValidFileType(fileType: string) {
+    return fileType.length > 0 && new RegExp('.[a-zA-Z]+$/').test(fileType);
+}
+
 export const reducer: RwaPortfolioAttachmentsOperations = {
     createAttachmentOperation(state, action, dispatch) {
-        // TODO: Implement "createAttachmentOperation" reducer
-        throw new Error('Reducer "createAttachmentOperation" not yet implemented');
+        if (
+            !action.input.id ||
+            !action.input.title ||
+            !action.input.fileType ||
+            !action.input.hash
+        ) {
+            throw new Error('Id, title, fileType, and hash are required');
+        }
+
+        if (
+            state.attachments.find(
+                attachment => attachment.id === action.input.id,
+            )
+        ) {
+            throw new Error(
+                `Attachment with id ${action.input.id} already exists`,
+            );
+        }
+
+        if (!isValidFileType(action.input.fileType)) {
+            throw new Error(`Invalid file type: ${action.input.fileType}`);
+        }
+
+        state.attachments.push({
+            ...action.input,
+        });
     },
     editAttachmentOperation(state, action, dispatch) {
-        // TODO: Implement "editAttachmentOperation" reducer
-        throw new Error('Reducer "editAttachmentOperation" not yet implemented');
+        if (
+            !action.input.id ||
+            !action.input.title ||
+            !action.input.fileType ||
+            !action.input.hash
+        ) {
+            throw new Error('Id, title, fileType, and hash are required');
+        }
+
+        if (
+            !state.attachments.find(
+                attachment => attachment.id === action.input.id,
+            )
+        ) {
+            throw new Error(
+                `Attachment with id ${action.input.id} does not exist`,
+            );
+        }
+
+        if (!isValidFileType(action.input.fileType)) {
+            throw new Error(`Invalid file type: ${action.input.fileType}`);
+        }
     },
     deleteAttachmentOperation(state, action, dispatch) {
-        // TODO: Implement "deleteAttachmentOperation" reducer
-        throw new Error('Reducer "deleteAttachmentOperation" not yet implemented');
+        if (!action.input.id) {
+            throw new Error('Id is required');
+        }
+
+        if (
+            !state.attachments.find(
+                attachment => attachment.id === action.input.id,
+            )
+        ) {
+            throw new Error(
+                `Attachment with id ${action.input.id} does not exist`,
+            );
+        }
+
+        state.attachments = state.attachments.filter(
+            attachment => attachment.id !== action.input.id,
+        );
     },
-}
+};
