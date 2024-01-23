76f80db9a25e4f86a58811068280acec133ce2d2
ryanwolhuter <dev@ryanwolhuter.com>
2024-01-23 10:46:23 +0200

    feat: add reducer and test for create group transaction
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/rwa-portfolio/src/reducers/transactions.ts
+++ document-models/rwa-portfolio/src/reducers/transactions.ts
@@ -4,25 +4,140 @@
  * - delete the file and run the code generator again to have it reset
  */
 
+import { z } from 'zod';
+import {
+    RwaBaseTransaction,
+    RwaGroupTransaction,
+    RwaPortfolioState,
+} from '../..';
+import { GroupTransactionTypeSchema } from '../../gen/schema/zod';
 import { RwaPortfolioTransactionsOperations } from '../../gen/transactions/operations';
+
+function validateRwaBaseTransaction(
+    state: RwaPortfolioState,
+    input: RwaBaseTransaction,
+) {
+    if (!input.id) {
+        throw new Error(`Transaction must have an id`);
+    }
+    if (state.transactions.find(transaction => transaction.id === input.id)) {
+        throw new Error(`Transaction with id ${input.id} already exists!`);
+    }
+    if (!input.asset) {
+        throw new Error(`Transaction must have an asset`);
+    }
+    if (!state.portfolio.find(asset => asset.id === input.asset)) {
+        throw new Error(`Asset with id ${input.asset} does not exist!`);
+    }
+    if (!input.amount) {
+        throw new Error(`Transaction must have an amount`);
+    }
+    const amount = z.number({
+        required_error: 'Amount is required',
+        invalid_type_error: 'Amount must be a positive number',
+    });
+
+    amount.positive({ message: 'Amount must be a positive' });
+    amount.finite({ message: 'Amount must be finite' });
+    amount.safe({
+        message:
+            'must be between Number.MIN_SAFE_INTEGER and Number.MAX_SAFE_INTEGER',
+    });
+    const dateSchema = z.coerce.date();
+    if (!input.entryTime) {
+        throw new Error(`Transaction must have an entry time`);
+    }
+    if (!dateSchema.safeParse(input.entryTime).success) {
+        throw new Error(`Entry time must be a valid date`);
+    }
+    if (input.tradeTime && !dateSchema.safeParse(input.tradeTime).success) {
+        throw new Error(`Trade time must be a valid date`);
+    }
+    if (
+        input.settlementTime &&
+        !dateSchema.safeParse(input.settlementTime).success
+    ) {
+        throw new Error(`Settlement time must be a valid date`);
+    }
+    if (input.account && !state.accounts.find(a => a.id === input.account)) {
+        throw new Error(`Account with id ${input.account} does not exist!`);
+    }
+    if (
+        input.counterParty &&
+        !state.accounts.find(a => a.id === input.counterParty)
+    ) {
+        throw new Error(
+            `Counter party account with id ${input.counterParty} does not exist!`,
+        );
+    }
+}
 
 export const reducer: RwaPortfolioTransactionsOperations = {
     createGroupTransactionOperation(state, action, dispatch) {
-        // TODO: Implement "createGroupTransactionOperation" reducer
-        throw new Error(
-            'Reducer "createGroupTransactionOperation" not yet implemented',
-        );
+        if (!action.input.id) {
+            throw new Error('Group transaction must have an id');
+        }
+        if (
+            state.transactions.find(
+                transaction => transaction.id === action.input.id,
+            )
+        ) {
+            throw new Error(
+                `Group transaction with id ${action.input.id} already exists!`,
+            );
+        }
+        if (!GroupTransactionTypeSchema.safeParse(action.input.type).success) {
+            throw new Error(`Invalid group transaction type`);
+        }
+        if (action.input.assetTransaction) {
+            try {
+                validateRwaBaseTransaction(
+                    state,
+                    action.input.assetTransaction,
+                );
+            } catch (e) {
+                console.error(`Invalid asset transaction: `, e);
+            }
+        }
+        if (action.input.cashTransaction) {
+            try {
+                validateRwaBaseTransaction(state, action.input.cashTransaction);
+            } catch (e) {
+                console.error(`Invalid cash transaction: `, e);
+            }
+        }
+        if (action.input.interestTransaction) {
+            try {
+                validateRwaBaseTransaction(
+                    state,
+                    action.input.interestTransaction,
+                );
+            } catch (e) {
+                console.error(`Invalid interest transaction: `, e);
+            }
+        }
+        if (action.input.feeTransactions?.length) {
+            action.input.feeTransactions.forEach(feeTransaction => {
+                if (feeTransaction) {
+                    try {
+                        validateRwaBaseTransaction(state, feeTransaction);
+                    } catch (e) {
+                        console.error(`Invalid fee transaction: `, e);
+                    }
+                }
+            });
+        }
+        if (action.input.attachments?.length) {
+            action.input.attachments.forEach(attachmentId => {
+                if (!state.attachments.find(a => a.id === attachmentId)) {
+                    throw new Error(
+                        `Attachment with id ${attachmentId} does not exist!`,
+                    );
+                }
+            });
+        }
+        state.transactions.push(action.input as RwaGroupTransaction);
     },
-    editGroupTransactionOperation(state, action, dispatch) {
-        // TODO: Implement "editGroupTransactionOperation" reducer
-        throw new Error(
-            'Reducer "editGroupTransactionOperation" not yet implemented',
-        );
-    },
-    deleteGroupTransactionOperation(state, action, dispatch) {
-        // TODO: Implement "deleteGroupTransactionOperation" reducer
-        throw new Error(
-            'Reducer "deleteGroupTransactionOperation" not yet implemented',
-        );
-    },
+    editGroupTransactionOperation(state, action, dispatch) {},
+    deleteGroupTransactionOperation(state, action, dispatch) {},
 };
--- document-models/rwa-portfolio/src/tests/transactions.test.ts
+++ document-models/rwa-portfolio/src/tests/transactions.test.ts
@@ -5,11 +5,11 @@
 
 import { generateMock } from '@powerhousedao/codegen';
 
-import utils from '../../gen/utils';
-import { z } from '../../gen/schema';
 import { reducer } from '../../gen/reducer';
+import { z } from '../../gen/schema';
 import * as creators from '../../gen/transactions/creators';
 import { RwaPortfolioDocument } from '../../gen/types';
+import utils from '../../gen/utils';
 
 describe('Transactions Operations', () => {
     let document: RwaPortfolioDocument;
@@ -19,17 +19,64 @@ describe('Transactions Operations', () => {
     });
 
     it('should handle createGroupTransaction operation', () => {
-        const input = generateMock(z.CreateGroupTransactionInputSchema());
+        const baseTransactionId = 'base-test-id';
+        const groupTransactionId = 'group-test-id';
+        const asset = generateMock(z.RwaAssetSchema());
+        const amount = 123;
+        const account = generateMock(z.RwaAccountSchema());
+        const counterParty = generateMock(z.RwaAccountSchema());
+        const txRef = 'tx-ref';
+        const entryTime = new Date().toISOString();
+        const tradeTime = new Date().toISOString();
+        const settlementTime = new Date().toISOString();
+        const baseTransaction = {
+            id: baseTransactionId,
+            asset: asset.id,
+            amount,
+            account: account.id,
+            counterParty: counterParty.id,
+            txRef,
+            entryTime,
+            tradeTime,
+            settlementTime,
+        };
+        const groupTransaction = {
+            id: groupTransactionId,
+            type: 'AssetPurchase',
+            assetTransaction: baseTransaction,
+            cashTransaction: baseTransaction,
+            interestTransaction: baseTransaction,
+            feeTransactions: [baseTransaction],
+            attachments: [] as string[],
+        };
+        const document = utils.createDocument({
+            state: {
+                global: {
+                    accounts: [account, counterParty],
+                    principalLender: account.id,
+                    spvs: [],
+                    feeTypes: [],
+                    fixedIncomeTypes: [],
+                    attachments: [],
+                    portfolio: [asset],
+                    transactions: [],
+                },
+                local: {},
+            },
+        });
         const updatedDocument = reducer(
             document,
-            creators.createGroupTransaction(input),
+            // @ts-expect-error mock
+            creators.createGroupTransaction(groupTransaction),
         );
 
         expect(updatedDocument.operations.global).toHaveLength(1);
         expect(updatedDocument.operations.global[0].type).toBe(
             'CREATE_GROUP_TRANSACTION',
         );
-        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(
+            groupTransaction,
+        );
         expect(updatedDocument.operations.global[0].index).toEqual(0);
     });
     it('should handle editGroupTransaction operation', () => {
