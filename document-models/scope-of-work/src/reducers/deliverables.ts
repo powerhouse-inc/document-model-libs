f6025870ef7e70726e70e2da53d2d1005d9c5361
liberuum <liberom@proton.me>
2024-03-25 19:05:51 +0000

    add all Deliverables operations

--- document-models/scope-of-work/src/reducers/deliverables.ts
+++ document-models/scope-of-work/src/reducers/deliverables.ts
@@ -1,5 +1,5 @@
 /**
- * This is a scaffold file meant for customization:
+ * This is a scaffold file meant for customization: 
  * - modify it by implementing the reducer functions
  * - delete the file and run the code generator again to have it reset
  */
@@ -8,8 +8,8 @@ import { ScopeOfWorkDeliverablesOperations } from '../../gen/deliverables/operat
 
 export const reducer: ScopeOfWorkDeliverablesOperations = {
     createDeliverableOperation(state, action, dispatch) {
-        state.deliverables.push({
-            id: action.input.id,
+        state.deliverables?.push({
+            id: action.input.id as string,
             title: action.input.title,
             status: 'TODO',
             owner: null,
@@ -20,4 +20,36 @@ export const reducer: ScopeOfWorkDeliverablesOperations = {
             keyResults: [],
         });
     },
-};
+    updateDeliverableProgressOperation(state, action, dispatch) {
+        // TODO: Implement "updateDeliverableProgressOperation" reducer
+        throw new Error('Reducer "updateDeliverableProgressOperation" not yet implemented');
+    },
+    deleteDeliverableOperation(state, action, dispatch) {
+        // TODO: Implement "deleteDeliverableOperation" reducer
+        throw new Error('Reducer "deleteDeliverableOperation" not yet implemented');
+    },
+    updateDeliverableStatusOperation(state, action, dispatch) {
+        // TODO: Implement "updateDeliverableStatusOperation" reducer
+        throw new Error('Reducer "updateDeliverableStatusOperation" not yet implemented');
+    },
+    updateDeliverableDetailsOperation(state, action, dispatch) {
+        // TODO: Implement "updateDeliverableDetailsOperation" reducer
+        throw new Error('Reducer "updateDeliverableDetailsOperation" not yet implemented');
+    },
+    addKeyResultToDeliverableOperation(state, action, dispatch) {
+        // TODO: Implement "addKeyResultToDeliverableOperation" reducer
+        throw new Error('Reducer "addKeyResultToDeliverableOperation" not yet implemented');
+    },
+    updateKeyResultOperation(state, action, dispatch) {
+        // TODO: Implement "updateKeyResultOperation" reducer
+        throw new Error('Reducer "updateKeyResultOperation" not yet implemented');
+    },
+    removeKeyResultFromDeliverableOperation(state, action, dispatch) {
+        // TODO: Implement "removeKeyResultFromDeliverableOperation" reducer
+        throw new Error('Reducer "removeKeyResultFromDeliverableOperation" not yet implemented');
+    },
+    setDeliverableBudgetOperation(state, action, dispatch) {
+        // TODO: Implement "setDeliverableBudgetOperation" reducer
+        throw new Error('Reducer "setDeliverableBudgetOperation" not yet implemented');
+    },
+}
--- document-models/scope-of-work/src/tests/deliverables.test.ts
+++ document-models/scope-of-work/src/tests/deliverables.test.ts
@@ -6,14 +6,10 @@
 import { generateMock } from '@powerhousedao/codegen';
 
 import utils from '../../gen/utils';
-import { CreateDeliverableInput, z } from '../../gen/schema';
+import { z } from '../../gen/schema';
 import { reducer } from '../../gen/reducer';
 import * as creators from '../../gen/deliverables/creators';
 import { ScopeOfWorkDocument } from '../../gen/types';
-
-function generateId(): string {
-    return (Math.random() + 1).toString(36).substring(7);
-}
 
 describe('Deliverables Operations', () => {
     let document: ScopeOfWorkDocument;
@@ -23,24 +19,127 @@ describe('Deliverables Operations', () => {
     });
 
     it('should handle createDeliverable operation', () => {
-        const input: CreateDeliverableInput = {
-            id: generateId(),
-            title: 'My new deliverable',
-        };
-
+        const input = generateMock(z.CreateDeliverableInputSchema());
         const updatedDocument = reducer(
             document,
             creators.createDeliverable(input),
         );
 
-        expect(updatedDocument.state.global.deliverables).toHaveLength(1);
-        expect(updatedDocument.state.global.deliverables[0].id).toEqual(
-            input.id,
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'CREATE_DELIVERABLE',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle updateDeliverableProgress operation', () => {
+        const input = generateMock(z.UpdateDeliverableProgressInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.updateDeliverableProgress(input),
         );
 
         expect(updatedDocument.operations.global).toHaveLength(1);
         expect(updatedDocument.operations.global[0].type).toBe(
-            'CREATE_DELIVERABLE',
+            'UPDATE_DELIVERABLE_PROGRESS',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle deleteDeliverable operation', () => {
+        const input = generateMock(z.DeleteDeliverableInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.deleteDeliverable(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'DELETE_DELIVERABLE',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle updateDeliverableStatus operation', () => {
+        const input = generateMock(z.UpdateDeliverableStatusInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.updateDeliverableStatus(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'UPDATE_DELIVERABLE_STATUS',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle updateDeliverableDetails operation', () => {
+        const input = generateMock(z.UpdateDeliverableDetailsInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.updateDeliverableDetails(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'UPDATE_DELIVERABLE_DETAILS',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle addKeyResultToDeliverable operation', () => {
+        const input = generateMock(z.AddKeyResultToDeliverableInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.addKeyResultToDeliverable(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'ADD_KEY_RESULT_TO_DELIVERABLE',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle updateKeyResult operation', () => {
+        const input = generateMock(z.UpdateKeyResultInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.updateKeyResult(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'UPDATE_KEY_RESULT',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle removeKeyResultFromDeliverable operation', () => {
+        const input = generateMock(z.RemoveKeyResultFromDeliverableInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.removeKeyResultFromDeliverable(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'REMOVE_KEY_RESULT_FROM_DELIVERABLE',
+        );
+        expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
+        expect(updatedDocument.operations.global[0].index).toEqual(0);
+    });
+    it('should handle setDeliverableBudget operation', () => {
+        const input = generateMock(z.SetDeliverableBudgetInputSchema());
+        const updatedDocument = reducer(
+            document,
+            creators.setDeliverableBudget(input),
+        );
+
+        expect(updatedDocument.operations.global).toHaveLength(1);
+        expect(updatedDocument.operations.global[0].type).toBe(
+            'SET_DELIVERABLE_BUDGET',
         );
         expect(updatedDocument.operations.global[0].input).toStrictEqual(input);
         expect(updatedDocument.operations.global[0].index).toEqual(0);
