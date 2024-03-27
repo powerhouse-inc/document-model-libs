4575b0feba1af602094252b8d0f4e6e5550196f9
ryanwolhuter <dev@ryanwolhuter.com>
2024-03-27 12:37:15 +0200

    feat: prefer h2
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- editors/rwa/other/other.tsx
+++ editors/rwa/other/other.tsx
@@ -7,29 +7,29 @@ import { SPVs } from './spvs';
 export const Other = (props: IProps) => {
     return (
         <div>
-            <h1 className="text-lg font-bold mb-2">Accounts</h1>
+            <h2 className="text-lg font-bold mb-2">Accounts</h2>
             <p className="text-xs text-gray-600 mb-4">
                 Add and manage accounts that can be associated with transactions
                 and fees.
             </p>
             <Accounts {...props} />
-            <h1 className="text-lg font-bold mt-4 mb-2">
+            <h2 className="text-lg font-bold mt-4 mb-2">
                 Fixed Income Asset Types
-            </h1>
+            </h2>
             <p className="text-xs text-gray-600 mb-4">
                 Add and manage fixed income asset types.
             </p>
             <FixedIncomeTypes {...props} />
-            <h1 className="text-lg font-bold mt-4 mb-2">
+            <h2 className="text-lg font-bold mt-4 mb-2">
                 Special Purpose Vehicles (SPVs)
-            </h1>
+            </h2>
             <p className="text-xs text-gray-600 mb-4">
                 Add and manage special purpose vehicles (SPVs).
             </p>
             <SPVs {...props} />
-            <h1 className="text-lg font-bold mt-4 mb-2">
+            <h2 className="text-lg font-bold mt-4 mb-2">
                 Service Provider Fee Types
-            </h1>
+            </h2>
             <p className="text-xs text-gray-600 mb-4">
                 Add and manage service providers and their associated fee types.
             </p>
