5eb0410eb1285e7aaca65ba06741ec671981bcd5
liberuum <liberom@proton.me>
2024-03-28 17:38:42 +0000

    updated reducers and doc model

--- document-models/scope-of-work/src/reducers/deliverables.ts
+++ document-models/scope-of-work/src/reducers/deliverables.ts
@@ -1,15 +1,17 @@
 /**
- * This is a scaffold file meant for customization: 
+ * This is a scaffold file meant for customization:
  * - modify it by implementing the reducer functions
  * - delete the file and run the code generator again to have it reset
  */
 
+import { isDeliverableProgress } from '../..';
+import { ScopeOfWorkState } from '../..';
 import { ScopeOfWorkDeliverablesOperations } from '../../gen/deliverables/operations';
 
 export const reducer: ScopeOfWorkDeliverablesOperations = {
     createDeliverableOperation(state, action, dispatch) {
         state.deliverables?.push({
-            id: action.input.id as string,
+            id: action.input.id,
             title: action.input.title,
             status: 'TODO',
             owner: null,
@@ -20,36 +22,130 @@ export const reducer: ScopeOfWorkDeliverablesOperations = {
             keyResults: [],
         });
     },
-    updateDeliverableProgressOperation(state, action, dispatch) {
-        // TODO: Implement "updateDeliverableProgressOperation" reducer
-        throw new Error('Reducer "updateDeliverableProgressOperation" not yet implemented');
+    updateDeliverableProgressOperation(
+        state: ScopeOfWorkState,
+        action,
+        dispatch,
+    ) {
+        const { deliverableId, workProgress } = action.input;
+
+        if (!isDeliverableProgress(workProgress)) {
+            throw new Error('Invalid workProgress');
+        }
+
+        const deliverable = state.deliverables?.find(
+            d => d?.id === deliverableId,
+        );
+
+        if (!deliverable) {
+            throw new Error('Deliverable not found');
+        }
+
+        deliverable.workProgress = workProgress;
     },
     deleteDeliverableOperation(state, action, dispatch) {
-        // TODO: Implement "deleteDeliverableOperation" reducer
-        throw new Error('Reducer "deleteDeliverableOperation" not yet implemented');
+        // throw error if state.deliverables is not defined
+        if (!state.deliverables) {
+            throw new Error('No deliverables found');
+        }
+        state.deliverables = state.deliverables.filter(
+            d => d?.id !== action.input.deliverableId,
+        );
     },
     updateDeliverableStatusOperation(state, action, dispatch) {
-        // TODO: Implement "updateDeliverableStatusOperation" reducer
-        throw new Error('Reducer "updateDeliverableStatusOperation" not yet implemented');
+        const deliverable = state.deliverables?.find(
+            d => d?.id === action.input.deliverableId,
+        );
+
+        if (!deliverable) {
+            throw new Error('Deliverable not found');
+        }
+
+        deliverable.status = action.input.status;
     },
     updateDeliverableDetailsOperation(state, action, dispatch) {
-        // TODO: Implement "updateDeliverableDetailsOperation" reducer
-        throw new Error('Reducer "updateDeliverableDetailsOperation" not yet implemented');
+        const deliverable = state.deliverables?.find(
+            d => d?.id === action.input.deliverableId,
+        );
+
+        if (!deliverable) {
+            throw new Error('Deliverable not found');
+        }
+
+        if (action.input.title !== undefined && action.input.title !== null) {
+            deliverable.title = action.input.title;
+        }
+        if (action.input.description !== undefined) {
+            deliverable.description = action.input.description;
+        }
     },
     addKeyResultToDeliverableOperation(state, action, dispatch) {
-        // TODO: Implement "addKeyResultToDeliverableOperation" reducer
-        throw new Error('Reducer "addKeyResultToDeliverableOperation" not yet implemented');
+        const { deliverableId, id, title, link } = action.input;
+
+        const deliverable = state.deliverables?.find(
+            d => d?.id === deliverableId,
+        );
+
+        if (!deliverable) {
+            throw new Error('Deliverable not found');
+        }
+
+        deliverable.keyResults.push({ id, title, link });
     },
     updateKeyResultOperation(state, action, dispatch) {
-        // TODO: Implement "updateKeyResultOperation" reducer
-        throw new Error('Reducer "updateKeyResultOperation" not yet implemented');
+        const { keyResultId, title, link } = action.input;
+
+        if (!state.deliverables) {
+            throw new Error('No deliverables found');
+        }
+        for (const deliverable of state.deliverables) {
+            const keyResult = deliverable?.keyResults.find(
+                kr => kr.id === keyResultId,
+            );
+
+            if (keyResult) {
+                if (title !== undefined) {
+                    keyResult.title = title;
+                }
+                if (link !== undefined && link !== null) {
+                    keyResult.link = link;
+                }
+                return;
+            }
+        }
+
+        throw new Error('Key result not found');
     },
     removeKeyResultFromDeliverableOperation(state, action, dispatch) {
-        // TODO: Implement "removeKeyResultFromDeliverableOperation" reducer
-        throw new Error('Reducer "removeKeyResultFromDeliverableOperation" not yet implemented');
+        const { deliverableId, keyResultId } = action.input;
+
+        if (!state.deliverables) {
+            throw new Error('No deliverables found');
+        }
+        for (const deliverable of state.deliverables) {
+            if (deliverable?.id === deliverableId) {
+                deliverable.keyResults = deliverable.keyResults.filter(
+                    kr => kr.id !== keyResultId,
+                );
+                return;
+            } else {
+                throw new Error(`KeyResult id: ${keyResultId} not found`);
+            }
+        }
     },
     setDeliverableBudgetOperation(state, action, dispatch) {
-        // TODO: Implement "setDeliverableBudgetOperation" reducer
-        throw new Error('Reducer "setDeliverableBudgetOperation" not yet implemented');
+        const { deliverableId, workUnitBudget, deliverableBudget } =
+            action.input;
+
+        const deliverable = state.deliverables?.find(
+            d => d?.id === deliverableId,
+        );
+
+        if (!deliverable) {
+            throw new Error('Deliverable not found');
+        }
+
+        deliverable.workUnitBudget = workUnitBudget;
+        deliverable.deliverableBudget = deliverableBudget;
     },
-}
+};
--- document-models/scope-of-work/src/utils.ts
+++ document-models/scope-of-work/src/utils.ts
@@ -1,1 +1,28 @@
-export {};
+type StoryPoints = {
+    total: number;
+    completed: number;
+};
+
+type Percentage = {
+    value: number;
+};
+
+type DeliverableProgress = StoryPoints | Percentage;
+
+export function isDeliverableProgress(
+    obj: DeliverableProgress,
+): obj is DeliverableProgress {
+    const isStoryPoints = (obj: StoryPoints): obj is StoryPoints =>
+        typeof obj.total === 'number' &&
+        obj.total > 0 &&
+        typeof obj.completed === 'number' &&
+        obj.completed >= 0 &&
+        obj.completed <= obj.total;
+
+    const isPercentage = (
+        obj: Percentage | null | undefined,
+    ): obj is Percentage =>
+        typeof obj?.value === 'number' && obj.value >= 0 && obj.value <= 1;
+
+    return isStoryPoints(obj as StoryPoints) || isPercentage(obj as Percentage);
+}
