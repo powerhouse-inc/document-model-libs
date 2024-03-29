bfa88e3c995ff642c5f5f552c9aa148647f6d538
liberuum <liberom@proton.me>
2024-03-29 13:11:05 +0000

    update model

--- document-models/scope-of-work/src/tests/deliverables.test.ts
+++ document-models/scope-of-work/src/tests/deliverables.test.ts
@@ -6,10 +6,14 @@
 import { generateMock } from '@powerhousedao/codegen';
 
 import utils from '../../gen/utils';
-import { z } from '../../gen/schema';
+import { CreateDeliverableInput, z } from '../../gen/schema';
 import { reducer } from '../../gen/reducer';
 import * as creators from '../../gen/deliverables/creators';
 import { ScopeOfWorkDocument } from '../../gen/types';
+
+function generateId(): string {
+    return (Math.random() + 1).toString(36).substring(7);
+}
 
 describe('Deliverables Operations', () => {
     let document: ScopeOfWorkDocument;
@@ -19,12 +23,19 @@ describe('Deliverables Operations', () => {
     });
 
     it('should handle createDeliverable operation', () => {
-        const input = generateMock(z.CreateDeliverableInputSchema());
+        const input: CreateDeliverableInput = {
+            id: generateId(),
+            title: 'My new deliverable',
+        };
+
         const updatedDocument = reducer(
             document,
             creators.createDeliverable(input),
         );
 
+        expect(updatedDocument.state.global.deliverables[0].id).toEqual(
+            input.id,
+        );
         expect(updatedDocument.operations.global).toHaveLength(1);
         expect(updatedDocument.operations.global[0].type).toBe(
             'CREATE_DELIVERABLE',
