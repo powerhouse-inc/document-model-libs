e4aa46c461c9899bd87cdd6542860ad4127347d0
wkampmann <wouter@ksdc.be>
2024-05-25 17:09:18 +0200

    Improved Scope of Work editor

--- editors/scope-of-work/editor.tsx
+++ editors/scope-of-work/editor.tsx
@@ -3,7 +3,6 @@ import {
     EditorToolbar,
     EditorWorksheet,
     TextInput,
-    ToolbarButton,
 } from 'document-model-libs/utils';
 import { EditorProps } from 'document-model/document';
 import {
@@ -23,10 +22,6 @@ export type IProps = EditorProps<
 export const randomId = () => {
     return Math.floor(Math.random() * Date.now()).toString(36);
 };
-
-function doNothing(message:string) {
-    return () => console.log(`${message} and did nothing`);
-}
 
 function ScopeOfWorkEditor(props: IProps) {
     const { document, dispatch, context } = props;
@@ -54,29 +49,81 @@ function ScopeOfWorkEditor(props: IProps) {
         }));
     }
 
+    const handleDeleteDeliverable = (id: string) => {
+        dispatch(actions.deleteDeliverable({ id }));
+    }
+
+    const handleToggleStatus = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+            dispatch(actions.updateDeliverableStatus({
+                id: event.target.value,
+                status: 'DELIVERED'
+            }));
+        
+        } else {
+            dispatch(actions.updateDeliverableStatus({
+                id: event.target.value,
+                status: 'TODO'
+            }));
+        }
+    }
+
+    const handleSetDocumentName = (name: string) => {
+        dispatch(actions.setName(name));
+    };
+
     return (
         <DocumentEditor mode={context.theme}>
             <EditorToolbar
                 key="toolbar" left={[]} center={[]} right={[]}
             />
             <EditorWorksheet key="sheet">
-                <label>Add deliverable:</label>
                 <TextInput
-                    key="doc-title"
-                    value={document.name}
+                        key="doc-title"
+                        value={document.name}
+                        size="huge"
+                        theme={context.theme}
+                        onSubmit={handleSetDocumentName}
+                    />
+                
+                <label>Add a deliverable:</label>
+                
+                <TextInput
+                    key="new-deliverable-title"
                     size="medium"
                     theme={context.theme}
                     onSubmit={handleCreateDeliverable}
+                    clearOnSubmit={true}
+                    autoFocus={true}
                 />
 
                 <table className='sow-deliverables-table'>
+                    <tbody>
                     {state.deliverables.map(d => (
-                        <tr>
-                            <td>{d.title}</td>
-                            <td>{d.status}</td>
-                            <td><input type="checkbox"/></td>
+                        <tr key={d.id}>
+                            <td key="checkbox">
+                                <input 
+                                    value={d.id} 
+                                    type="checkbox" 
+                                    onChange={handleToggleStatus}
+                                    checked={d.status == 'DELIVERED'}
+                                    />
+                            </td>
+                            <td key="title">{d.title}</td>
+                            <td key="status">
+                                <span className={[
+                                    'sow-deliverables-status',
+                                    'sow-deliverables-status--' + d.status.toLowerCase()
+                                ].join(' ')}>{d.status}</span>
+                            </td>
+                            <td key="actions" className="sow-deliverables-table--columnActions">
+                                <button className="sow-deliverables-delete" onClick={() => handleDeleteDeliverable(d.id)}>
+                                    &times;
+                                </button>
+                            </td>
                         </tr>
                     ))}
+                    </tbody>
                 </table>
 
             </EditorWorksheet>
