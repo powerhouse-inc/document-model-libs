28234dca0590cd235eed73fc104929363dd81683
Benjamin Jordan <benjamin@thegoldenmule.com>
2024-08-01 08:59:45 -0500

    feat: added total grant size to summary tab

--- editors/arb-ltip/components/GranteeStats.tsx
+++ editors/arb-ltip/components/GranteeStats.tsx
@@ -31,7 +31,7 @@ const GranteeStats = ({ state, onOpenHistorical }: GranteeStatsProps) => {
     const totalArb = state.grantSize || 0;
     return (
         <div>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-4">
                 <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
                     <dt className="truncate text-sm font-medium text-gray-500 flex items-baseline">
                         Days Remaining
@@ -39,6 +39,15 @@ const GranteeStats = ({ state, onOpenHistorical }: GranteeStatsProps) => {
                     </dt>
                     <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
                         {daysRemaining}
+                    </dd>
+                </div>
+                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+                    <dt className="truncate text-sm font-medium text-gray-500 flex items-baseline">
+                        Grant Size (ARB)
+                        <InfoTooltip text="The total grant size, in ARB." />
+                    </dt>
+                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+                        {totalArb}
                     </dd>
                 </div>
                 <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
